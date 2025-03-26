8a3ab9ef6af5cd38759de965f3f4d220ae8accb5
Gundam <kcheung00@hotmail.com>
2025-03-25 19:33:44 -0700

    initial email sample

---
+++ src/test/java/com/gundam/EmailSampleTest.java
@@ -0,0 +1,19 @@
+package com.gundam;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple EmailSample.
+ */
+public class EmailSampleTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
